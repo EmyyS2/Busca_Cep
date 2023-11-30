0760484f491d124892808a1c6cb58e4462e22816
EmyyS2 <miacontapcsenai@gmail.com>
2023-11-30 09:49:16 -0300

    mensagens

--- src/components/Cadastro.tsx
+++ src/components/Cadastro.tsx
@@ -1,17 +1,25 @@
-import React, {Component, useState, ChangeEvent, FormEvent, useEffect}from 'react';
+import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import styles from '../App.module.css'
 import Footer from './Footer';
 
-const Cadastro =()=>{
+const Cadastro = () => {
 
     const [nome, setNome] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [cpf, setCpf] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [nomeError, setNomeError] = useState<string>("");
+    const [emailError, setEmailError] = useState<string>("");
+    const [cpfError, setCpfError] = useState<string>("");
+    const [passwordError, setPasswordError] = useState<string>("");
 
     const cadastrarUsuario = (e: FormEvent) => {
+        setNomeError("")
+        setEmailError("")
+        setCpfError("")
+
         e.preventDefault();
         const dados = {
             nome: nome,
@@ -21,14 +29,32 @@ const Cadastro =()=>{
         }
 
         console.log(dados)
-        axios.post('http://10.137.9.131:8000/api/store', dados, {
-            headers:
-                { "Accept": "application/json", "Content-Type": "application/json" }
-        }).then(function (response) {
-            window.location.href = "/listagem"
-        }).catch(function (error) {
-            console.log(error);
-        });
+        axios.post('http://10.137.9.136:8000/api/store',
+            dados,
+            {
+                headers:
+                    { "Accept": "application/json", "Content-Type": "application/json" }
+            }).then(function (response) {
+                if (response.data.success === false) {
+                    if ('nome' in response.data.error) {
+                        setNomeError(response.data.error.nome[0])
+                    }
+                    if ('email' in response.data.error) {
+                        setEmailError(response.data.error.email[0])
+                    }
+                    if ('cpf' in response.data.error) {
+                        setCpfError(response.data.error.cpf[0])
+                    }
+                    if ('password' in response.data.error) {
+                        setPasswordError(response.data.error.password[0])
+                    }
+
+                }else {
+                window.location.href = "/listagem"
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
     }
 
     const handleState = (e: ChangeEvent<HTMLInputElement>) => {
@@ -57,19 +83,20 @@ const Cadastro =()=>{
                             <form onSubmit={cadastrarUsuario} className='row g-3'>
                                 <div className='col-6'>
                                     <label htmlFor="nome" className='form-label'>Nome</label>
-                                    <input type="text" name='nome' className='form-control' required onChange={handleState} />{nome}
+                                    <input type="text" name='nome' className='form-control' required onChange={handleState} /> <div className='text-danger'>{nomeError}</div>
+
                                 </div>
                                 <div className='col-6'>
                                     <label htmlFor="email" className='form-label'>E-mail</label>
-                                    <input type="text" name='email' className='form-control' required onChange={handleState}/>
+                                    <input type="text" name='email' className='form-control' required onChange={handleState} /><div className='text-danger'>{emailError}</div>
                                 </div>
                                 <div className='col-6'>
                                     <label htmlFor="cpf" className='form-label'>CPF</label>
-                                    <input type="text" name='cpf' className='form-control' required onChange={handleState}/>
+                                    <input type="text" name='cpf' className='form-control' required onChange={handleState} /><div className='text-danger'>{cpfError}</div>
                                 </div>
                                 <div className='col-6'>
                                     <label htmlFor="password" className='form-label'>Senha</label>
-                                    <input type="text" name='password' className='form-control' required onChange={handleState} />
+                                    <input type="text" name='password' className='form-control' required onChange={handleState} /><div className='text-danger'>{passwordError}</div>
                                 </div>
                                 <div className='col-12'>
                                     <button type='submit' className='btn btn-dark btn-sm'>Cadastrar</button>
